8deef66421cd92c8236c4d5c4da37ab92c7ef416
AssanCred <temix@live.ru>
2023-05-07 17:44:47 +0700

    Initial Commit

---
+++ HW_R&M_Characters_LatushkinAS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_R&M_Characters_LatushkinAS
+//
+//  Created by Артём Латушкин on 07.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
